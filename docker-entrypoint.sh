26ce5f680b8f6b877b98bcbed2a286b4dcdb3cf6
Ana Julia Castelo Branco Fernandes <anajuliacbfernandes@gmail.com>
2024-11-14 15:10:44 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Substitui o placeholder ${INTERNALTEXT} no index.html
-envsubst '$INTERNALTEXT' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
+envsubst '$INTERNALTEXT' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
 
 # Substitui o placeholder ${INTERNALTEXT} no nginx.conf.template (se necessário)
 envsubst '$INTERNALTEXT' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
