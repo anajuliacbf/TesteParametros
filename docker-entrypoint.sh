dbd402c90dad413b2837c4b17315512fe1877853
Ana Julia Castelo Branco Fernandes <anajuliacbfernandes@gmail.com>
2024-11-14 15:22:36 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# Verifica se a variável está presente
+echo "Valor de INTERNALTEXT: $INTERNALTEXT"
+
 # Substitui o placeholder ${INTERNALTEXT} no index.html
 envsubst '$INTERNALTEXT' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
 
+# Exibe o resultado para verificação
+cat /usr/share/nginx/html/index.html
+
 # Executa o Nginx
 exec "$@"
+
+
+#!/bin/sh
