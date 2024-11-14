8aee1f9f90e15bf7190b17a2b96cfb01cf1d8bad
anajuliacbf <anajuliacbfernandes@gmail.com>
2024-11-14 14:54:20 -0300

    Criação do script de inicialização docker-entrypoint.sh, que substitui o placeholder $\{INTERNALTEXT\} no index.html

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Substitui o placeholder ${INTERNALTEXT} no index.html
+envsubst '$INTERNALTEXT' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
+
+# Substitui o placeholder ${INTERNALTEXT} no nginx.conf.template (se necessário)
+envsubst '$INTERNALTEXT' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+
+# Executa o Nginx
+exec "$@"
