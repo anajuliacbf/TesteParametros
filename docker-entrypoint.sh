262379e11f90d4684249b0a0a0b3f88fb5a8c26e
Ana Julia Castelo Branco Fernandes <anajuliacbfernandes@gmail.com>
2024-11-14 14:32:05 -0300

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Substitui o placeholder ${INTERNALTEXT} pelo valor da variável de ambiente INTERNALTEXT
-envsubst '$INTERNALTEXT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Executa o Nginx
-exec "$@"
