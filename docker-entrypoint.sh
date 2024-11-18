b5e13c4c70fd2b8a50fa6a8ae3fe5f5a4ff2fdd8
Ana Julia Castelo Branco Fernandes <anajuliacbfernandes@gmail.com>
2024-11-18 12:07:40 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,10 @@
 echo "Valor de INTERNALTEXT: $INTERNALTEXT"
 
 # Substitui o placeholder ${INTERNALTEXT} no index.html
-envsubst '$INTERNALTEXT' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
+envsubst < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index2.html
+
+# Copia o conteúdo do index2 para o index
+cp /usr/share/nginx/html/index2.html /usr/share/nginx/html/index.html
 
 # Exibe o resultado para verificação
 cat /usr/share/nginx/html/index.html
